b629176e4550ecd242bd01e4b654acf668d590c8
MansourMoukdad88 <m_sy@outlook.com>
2019-05-27 21:23:10 +0300

    work on addToCart function in context.js and make sure the button works correctly

--- src/context.js
+++ src/context.js
@@ -8,7 +8,8 @@ const ProductContext = React.createContext()
 class ProductProvider extends Component {
   state = {
     products: [],
-    detailProduct:detailProduct
+    detailProduct:detailProduct,
+    cart: []
   }
   componentDidMount(){
     this.setProducts()
@@ -34,8 +35,23 @@ class ProductProvider extends Component {
     })
   } 
   addToCart = (id) => {
-    console.log(`Hello from add To Cart"  : ${id}`);
-  } 
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+
+    this.setState(()=>{
+      return {
+        product: tempProducts,
+        cart: [...this.state.cart, product]
+      }
+    }, ()=>{console.log(this.state);
+    })
+    console.log(product.total);        
+  }
 
   render() {
     return (
